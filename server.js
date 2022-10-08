9ff4d65f1f05c83dd9065848b09c2a3d099818d3
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-10-07 22:43:50 -0600

    feat: express-session implemented

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const sequelize = require('./config/connection.js');
 const routes = require('./controllers/index.js');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 
 const PORT = process.env.PORT || 3001;
 
+// setting up express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const hbs = exphbs.create();
 
+
+// express session config
+const sess = {
+  secret: process.env.SC_KEY,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+};
+
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
