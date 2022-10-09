13b22fc94515955f99784998bc3aac2f8acc7c58
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-10-08 19:08:43 -0600

    fix: api route endpoint

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,7 @@ const userRoutes = require('./api/userRoutes.js')
 const homeRoutes = require('./homeRoutes');
 
 router.use('/api/notes', notesRoutes)
-router.use('./api/user', userRoutes);
+router.use('/api/user', userRoutes);
 router.use('/', homeRoutes);
 
 module.exports = router;
