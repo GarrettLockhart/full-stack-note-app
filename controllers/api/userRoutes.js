d3f5d5eef323cdd22ed3e62385d97e734cf5e51d
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-10-08 20:40:09 -0600

    feat: login implemented, and logout

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -52,4 +52,51 @@ router.post('/', async (req, res) => {
   }
 });
 
+
+// logs user in
+router.post('/login', async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
+    
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect password. Please try again!' });
+      return;
+    }
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+// logs user out
+router.post('/logout', async (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 module.exports = router;
