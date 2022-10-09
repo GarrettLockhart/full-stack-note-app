2d1edade58c5ed56ae506903d419ec21f4995636
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-10-08 19:08:29 -0600

    fix: sequelize sync not reseting tables

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const sequelize = require('./config/connection.js');
 const routes = require('./controllers/index.js');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+require ("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 
@@ -14,7 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const hbs = exphbs.create();
-
 
 // express session config
 const sess = {
@@ -31,6 +31,6 @@ app.set('view engine', 'handlebars');
 
 app.use(routes);
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 });
