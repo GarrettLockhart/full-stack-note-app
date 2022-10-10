ea99d0740f8d845afc62142efa253a940f1aa6ae
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-10-10 17:15:20 -0600

    feat: get posts from db, and display on homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,26 @@
 const withAuth = require('../utils/helpers');
+const Post = require('../models/Post.js');
+const sequelize = require('../config/connection');
 
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.render('homepage');
+router.get('/', async (req, res) => {
+  try {
+    const dbPostsData = await Post.findAll({
+      attributes: {
+        exclude: ['id', 'content', 'post_creator']
+      }
+    });
+
+    const posts = dbPostsData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', {
+      posts
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get('/dashboard', withAuth, (req, res) => {
